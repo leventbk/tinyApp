8cf83f92e1e1a13296840b0d72cb5c4fb8fae80b
leventkarakus <leventbkarakus@hotmail.com>
2023-06-22 12:01:44 -0400

    GET method for urlDatabase

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-const urlDatabase = {
+const urlDatabase = { //JSON object
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -10,6 +10,10 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get('/urls.json', (req, res) => {
+	res.json(urlDatabase)
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
