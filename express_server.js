8ca868f35a68e8cf2889c2d4f2b5a8b5dd56a6d8
leventkarakus <leventbkarakus@hotmail.com>
2023-06-22 20:21:35 -0400

    Sending Data to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -17,14 +17,11 @@ app.get('/urls.json', (req, res) => {
 	res.json(urlDatabase)
 })
 
-app.get("/set", (req, res) => {
-	const a = 1;
-	res.send(`a = ${a}`);
-   });
-   
-   app.get("/fetch", (req, res) => {
-	res.send(`a = ${a}`);
-   });
+app.get("/urls", (req, res) => {
+	const templateVars = { urls: urlDatabase };
+	res.render("urls_index", templateVars)
+})
+
 app.get('/hello', (req, res) => {
 	res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
