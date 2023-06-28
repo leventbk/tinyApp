35ae4ee17f1cf48f5a382c2bb3fabeaa71acc030
leventkarakus <leventbkarakus@hotmail.com>
2023-06-28 14:27:21 -0400

    working on edit with new views

--- express_server.js
+++ express_server.js
@@ -1,17 +1,17 @@
 function generateShortURL() {
-	let shortURL = '';
-	const length = 6;
-	const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let shortURL = '';
+  const length = 6;
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
  
-	for (let i = 0; i < length; i++) {
-		const randomIndex = Math.floor(Math.random() * characters.length);
-		shortURL += characters[randomIndex];
-	}
-	return shortURL;
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    shortURL += characters[randomIndex];
+  }
+  return shortURL;
 }
 
 const express = require('express');
-const cookieParser = require('cookie-parser'); 
+//const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -25,46 +25,49 @@ const urlDatabase = { //JSON object
 //middleware app.use
 app.use(express.urlencoded({ extended: true})); //will convert the request body from a Buffer into string (If you find that req.body is undefined, it may be that the body-parser middleware is not being run correctly.)
 
-app.use(cookieParser());
+//app.use(cookieParser());
 
 
 app.get("/", (req, res) => {
-	res.send("Hello!");
+  res.send("Hello!");
 });
 // Create
 app.post("/urls", (req, res) => {
-	let longURL = req.body.longURL;
-	const shortURL = generateRandomString();
-	urlDatabase[shortURL] = longURL;
-	res.redirect(`/urls/${shortURL}`);
-  });
-
-app.get('/urls.json', (req, res) => {
-	res.json(urlDatabase)
-})
-
-//OK
+  let longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+//OK read
 app.get("/urls", (req, res) => {
 	const templateVars = { urls: urlDatabase };
-	res.render("urls_index", templateVars)
-})
+	res.render("urls_index", templateVars);
+});
+  
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
 
 //OK
 app.get("/urls/new", (req, res) => {
-	res.render("urls_new");
-})
+  res.render("urls_new");
+});
 
 // app.get('/urls/:id', (req, res) => {
-// 	const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
-// 	res.render('urls_show', templateVars);
-//   });
+//   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+//   res.render('urls_show', templateVars);
+// });
+app.get('/urls/:id', (req, res) => {
+	const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+	res.render('urls_show', templateVars);
+  });
 
-
-
+//update
 app.get("/urls/:id", (req, res) => {
-	const id = req.params.id;
-	const templateVars = { id: id, longURL: urlDatabase[id]};
-	res.render("urls_show", templateVars);
+  const id = req.params.id;
+  const templateVars = { id: id, longURL: urlDatabase[id]};
+  res.render("urls_show", templateVars);
 });
 
 // app.post("/urls", (req, res) => {
@@ -73,26 +76,48 @@ app.get("/urls/:id", (req, res) => {
 //   });
 //create
 app.post("/urls", (req, res) => {
-	let longURL = req.body.longURL;
-	const shortURL = generateRandomString();
-	urlDatabase[shortURL] = longURL;
-	res.redirect(`/urls/${shortURL}`);
-  });
+  let longURL = req.body.longURL;
+  const shortURL = generateShortURL();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
-//OK
+// Delete
 app.post("/urls/:id/delete", (req, res) => {
-	delete urlDatabase[req.params.id]
-	console.log(req.body);
-	res.redirect('/urls')
+  delete urlDatabase[req.params.id];
+  console.log(req.body);
+  res.redirect('/urls');
+});
 
-})
-
+app.get('/u/:id', (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
+  if (!longURL) {
+	  res.status(404).send("Not found");
+  } else {
+	  res.redirect(longURL);
+  }
+});
 
 //UPDATE
 app.post('/urls/:id', (req, res) => {
-	urlDatabase[req.params.id] = req.body.editURL;
-	res.redirect('/urls');
-  });
+  urlDatabase[req.params.id] = req.body.editURL;
+  res.redirect('/urls');
+});
+
+
+// /login
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+  
+// logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
